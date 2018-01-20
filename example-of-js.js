2e1d03d86149f0c8d32f26dc92f22005eeb25ae5
aekirgil <ahmetemrekirgil@gmail.com>
2018-01-20 16:00:39 +0300

    deleted example file

--- example-of-js.js
+++
@@ -1,9 +0,0 @@
-const positions = ['First', 'Second', 'Fifth', 'Sixth', 'Seventh'];
-const morePositions = [
-  ...positions.slice(0, 2),
-  'Third',
-  'Fourth',
-  ...positions.slice(2)
-];
-
-console.log(morePositions);
